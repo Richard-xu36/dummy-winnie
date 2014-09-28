e84111aa454193a2562be1046fb76e39e9370907
richard <richard.xu36@gmail.com>
2014-09-28 08:13:40 +0000

    yue

---
+++ src/main/java/hello/BadRequestException.java
@@ -0,0 +1,11 @@
+package hello;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(value=HttpStatus.BAD_REQUEST, reason="Malformed request")  // 400
+public class BadRequestException extends RuntimeException {
+  public BadRequestException(String err) {
+     super(err);
+  }
+}
